b2569689e746dc1bfbbb7033b2360278041cf054
IU <68397119+WikiLiu@users.noreply.github.com>
2023-04-19 01:16:17 +0800

    Update operation.sh

--- operation.sh
+++ operation.sh
@@ -15,3 +15,24 @@ xz -d node-v18.16.0-linux-x64.tar.xz
 tar -xvf node-v18.16.0-linux-x64.tar
 cd /etc/bin
 sudo ln -s  ~/node-v18.16.0-linux-x64/bin/node node
+
+
+
+//install vim
+sudo apt-get update
+sudo apt-get install -y build-essential libncurses-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python3-dev
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=no \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=/usr/lib/python3.9/config-3.9-x86_64-linux-gnu \
+            --enable-perlinterp=no \
+            --enable-luainterp=no \
+            --enable-gui=no \
+            --enable-cscope \
+            --prefix=/usr/local
+
+make
+sudo make install
